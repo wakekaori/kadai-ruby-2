660badbb66a7bedad70c6bdbc974c5a8c6e6a468
wakekaori <wakekaoridevelopment@gmail.com>
2023-05-04 01:52:01 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,24 @@
+def fizzbuzz(num)
+  
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0 
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
+  
+end
+
+
+num_max = 100
+i = 1
+
+while i <= num_max 
+
+  puts fizzbuzz i
+  i += 1
+
+end
